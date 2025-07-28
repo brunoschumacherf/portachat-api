18d614aeabe6a3098f4b2013a5b6eb5e12f8460b
Bruno Schumacher <brunoschumacherfsouza@gmail.com>
2025-07-28 10:06:23 -0300

    feat(POR-14): config to deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
+    origins 'http://localhost:5173',
+            'https://portachat-client.vercel.app/login'
 
     resource '*',
       headers: :any,
